79a06f8d63135b8db5b4dc56703266fa0ac4063d
markthos <aydenthos@gmail.com>
2024-01-30 22:06:19 -0800

    mongodb variable connection verfication

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -1,12 +1,24 @@
-// mongooseConnection.mjs (ES module syntax)
+// connection.mjs
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/BrewDOCS",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+mongoose.connect(MONGODB_URI || "mongodb://127.0.0.1:27017/BrewDOCS",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 );
 
-export default mongoose.connection;
+const connection = mongoose.connection;
+
+connection.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+connection.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+export { connection as mongooseConnection };
