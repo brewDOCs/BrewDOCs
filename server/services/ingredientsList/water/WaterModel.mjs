ce0bb0b66656ef3bf1c4519456bc8440731a1f8f
markthos <aydenthos@gmail.com>
2024-02-10 09:14:09 -0800

    setup CRUD for water and add metrics to model

--- server/services/ingredientsList/water/WaterModel.mjs
+++ server/services/ingredientsList/water/WaterModel.mjs
@@ -8,6 +8,21 @@ const WaterSchema = new Schema({
     required: true,
     trim: true,
   },
+  waterCalcium: {
+    type: Number,
+  },
+  waterMagnesium: {
+    type: Number,
+  },
+  waterSodium: {
+    type: Number,
+  },
+  waterSulfate: {
+    type: Number,
+  },
+  waterChloride: {
+    type: Number,
+  },
   waterAmount: {
     type: Number,
   },
