c1e1b8692febd527c5e77eff33f75b93ebbed9fb
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-02-10 19:18:08 -0800

    Add basic CRUD to clarification step service

--- server/services/process/clarification/ClarificationModel.mjs
+++ server/services/process/clarification/ClarificationModel.mjs
@@ -5,23 +5,49 @@ import { Schema, model } from "mongoose";
 const ClarificationSchema = new Schema({
   clarificationDescription: {
     type: String,
+    required: true,
     trim: true,
   },
   clarificationTemperature: {
     type: Number,
+    required: true,
   },
   clarificationStartTime: {
     type: Date,
     default: Date.now,
+    get: (date) => {
+      return date.toISOString();
+    },
   },
   clarificationEndTime: {
     type: Date,
     default: Date.now,
+    get: (date) => {
+      return date.toISOString();
+    },
   },
-  additives: {
-    type: Schema.Types.ObjectId,
-    ref: "Additives",
+  clarificationElapsedTime: {
+    type: Number,
   },
+  clarificationNotificationTime: {
+    type: Number,
+  },
+  additives: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Additives",
+    },
+  ],
+});
+
+// Calculates elapsed time in seconds using mongoose pre-save middleware, NOTE: does not run on update() or findOneandUpdate()
+// 'this' refers to the document about to be saved
+ClarificationSchema.pre("save", function (next) {
+  if (this.clarificationStartTime && this.clarificationEndTime) {
+    this.clarificationElapsedTime =
+      (this.clarificationEndTime - this.clarificationStartTime) / 1000; // in seconds
+  }
+  next();
 });
 
 const ClarificationModel = model("Clarification", ClarificationSchema);
