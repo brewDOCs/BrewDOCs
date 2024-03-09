225be175257bbce9bbf654b9536eefb20da79b3f
markthos <aydenthos@gmail.com>
2024-03-09 12:55:28 -0800

    removed the src folder

--- brewdocs/tailwind.config.js
+++ brewdocs/tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
