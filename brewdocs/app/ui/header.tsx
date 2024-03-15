aa7a4050aef1117d29f059510e6167d2c44ad0b5
Gagucci <126989750+Gagucci@users.noreply.github.com>
2024-03-14 19:06:55 -0700

    added file structure for ui/ components, starter code for Header

---
+++ brewdocs/app/ui/header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default async function Header() {
+  return (
+    <header className="text-gray-600 body-font border-2 border-red-500">
+      <div className="container mx-auto flex flex-wrap md:flex-row  justify-between items-center border-2 border-slate-500">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <span className="ml-3 text-xl">Tailblocks</span>
+        </a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900">First Link</a>
+          <a className="mr-5 hover:text-gray-900">Second Link</a>
+          <a className="mr-5 hover:text-gray-900">Third Link</a>
+          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+        </nav>
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          Button
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+}
