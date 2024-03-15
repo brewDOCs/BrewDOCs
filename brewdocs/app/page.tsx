f6ffe5f409db827a35eb5cc059c21e1444df76dc
Gagucci <126989750+Gagucci@users.noreply.github.com>
2024-03-14 19:36:48 -0700

    fixed problem with header

--- brewdocs/app/page.tsx
+++ brewdocs/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import Header from "./ui/header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
--- brewdocs/app/ui/header.tsx
+++ brewdocs/app/ui/header.tsx
@@ -2,19 +2,18 @@ import React from "react";
 
 export default async function Header() {
   return (
-    <header className="text-gray-600 body-font border-2 border-red-500">
-      <div className="container mx-auto flex flex-wrap md:flex-row  justify-between items-center border-2 border-slate-500">
+    <header className="text-gray-600 body-font border-2 border-red-500 ">
+      <div className="container mx-auto flex flex-wrap md:flex-row  justify-between space-x-2.5 items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">BrewDocs</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          <a className="mr-5 hover:text-gray-900">About</a>
+          <a className="mr-5 hover:text-gray-900">Contact Us</a>
+          <a className="mr-5 hover:text-gray-900">Login</a>
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Button
+          Get Started!
           <svg
             fill="none"
             stroke="currentColor"
