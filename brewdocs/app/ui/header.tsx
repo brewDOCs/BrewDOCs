33d2febf235f8cab193061439ceb95f689f50302
Gagucci <126989750+Gagucci@users.noreply.github.com>
2024-03-15 17:13:16 -0700

    fixed bg color, cards, and header

--- brewdocs/app/ui/header.tsx
+++ brewdocs/app/ui/header.tsx
@@ -2,24 +2,24 @@ import React from "react";
 
 export default async function Header() {
   return (
-    <header className="text-gray-600 body-font border-2 border-red-500 ">
-      <div className="container mx-auto flex flex-wrap md:flex-row  justify-between space-x-2.5 items-center">
+    <header className="text-gray-600 body-font bg-slate-300/35 ">
+      <div className="container mx-auto py-2 flex flex-wrap md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <span className="ml-3 text-xl">BrewDocs</span>
         </a>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">About</a>
-          <a className="mr-5 hover:text-gray-900">Contact Us</a>
-          <a className="mr-5 hover:text-gray-900">Login</a>
+        <nav className="md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a className="ml-5 hover:text-gray-900">About</a>
+          <a className="ml-5 hover:text-gray-900">Contact Us</a>
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Get Started!
+        <a className="mr-5 hover:text-gray-900">Login</a>
+        <button className="inline-flex items-center bg-amber-200 border-0 py-1 px-3 focus:outline-none hover:bg-amber-300 rounded text-base mt-4 md:mt-0">
+          Get Started
           <svg
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             className="w-4 h-4 ml-1"
             viewBox="0 0 24 24"
           >
